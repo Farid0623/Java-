ebdb26596980559ba9b7d07087e16a8f6c6800a6
Farid0623 <132091536+Farid0623@users.noreply.github.com>
2023-07-13 07:54:08 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+
+
+
+
+public class persona {
+
+    public String nombre;
+    public int edad;
+    public double estatura;
+    public char sexo;
+    // construir sin parámetros
+    public persona ()
+    {
+        this.nombre =    "";
+        this.edad = 0;
+        this.estatura = 0;
+        this.sexo = 'm';
+    }
+    public persona (String nombre, int edad, double estatura, char sexo);
+    {
+    public String getNombre(){
+
+    }
+    return nombre;
+    }
+}
+
+
+
